b6d9998af659ad1ac1b539d0cd51f726db18c771
Alulestad <pasante@iepi.gov.ec>
2024-08-20 12:13:58 -0500

    formulario y notificaciones

---
+++ src/main/java/com/senadi/pasantes/intranet/repository/modelo/Formulario.java
@@ -0,0 +1,93 @@
+package com.senadi.pasantes.intranet.repository.modelo;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "formulario")
+public class Formulario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_formulario")
+	@SequenceGenerator(name = "seq_formulario", sequenceName = "seq_formulario", allocationSize = 1)
+	@Column(name = "form_id")
+	private Integer id;
+	@Column(name = "form_fecha_emision")
+	private LocalDateTime fechaEmision;
+	@Column(name = "form_fecha_tratamiento")
+	private LocalDateTime fechaTratamiento;
+	@Column(name = "form_estado")
+	private String estado;
+
+	
+	@ManyToOne()
+	@JoinColumn(name = "ntfc_id_emisor")
+	private Usuario emisor;
+	
+	@ManyToOne()
+	@JoinColumn(name = "ntfc_id_destinatario")
+	private Usuario destinatario;
+
+	
+	//gets y sets
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getFechaEmision() {
+		return fechaEmision;
+	}
+
+	public void setFechaEmision(LocalDateTime fechaEmision) {
+		this.fechaEmision = fechaEmision;
+	}
+
+	public LocalDateTime getFechaTratamiento() {
+		return fechaTratamiento;
+	}
+
+	public void setFechaTratamiento(LocalDateTime fechaTratamiento) {
+		this.fechaTratamiento = fechaTratamiento;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Usuario getEmisor() {
+		return emisor;
+	}
+
+	public void setEmisor(Usuario emisor) {
+		this.emisor = emisor;
+	}
+
+	public Usuario getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(Usuario destinatario) {
+		this.destinatario = destinatario;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/senadi/pasantes/intranet/repository/modelo/Notificacion.java
@@ -0,0 +1,90 @@
+package com.senadi.pasantes.intranet.repository.modelo;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "notificacion")
+public class Notificacion {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_notificacion")
+	@SequenceGenerator(name = "seq_notificacion", sequenceName = "seq_notificacion", allocationSize = 1)
+	@Column(name = "ntfc_id")
+	private Integer id;
+	@Column(name = "ntfc_fecha_inicio")
+	private LocalDateTime fechaInicio;
+	@Column(name = "ntfc_fecha_fin")
+	private LocalDateTime fechaFin;
+	@Column(name = "ntfc_url_imagen")
+	private String urlImagen;
+	@Column(name = "ntfc_importancia")
+	private String importancia;
+	@Column(name = "ntfc_estado")
+	private String estado;
+	
+	@ManyToOne()
+	@JoinColumn(name = "ntfc_id_usuario")
+	private Usuario usuario;
+	
+	
+	
+	//Get y set
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public LocalDateTime getFechaInicio() {
+		return fechaInicio;
+	}
+	public void setFechaInicio(LocalDateTime fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+	public LocalDateTime getFechaFin() {
+		return fechaFin;
+	}
+	public void setFechaFin(LocalDateTime fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+	public String getUrlImagen() {
+		return urlImagen;
+	}
+	public void setUrlImagen(String urlImagen) {
+		this.urlImagen = urlImagen;
+	}
+	public String getImportancia() {
+		return importancia;
+	}
+	public void setImportancia(String importancia) {
+		this.importancia = importancia;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+	
+	
+
+}
